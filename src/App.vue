a66f31e1a6c078cf9157e7cd4b2964bbcc56d563
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2023-06-27 15:14:45 -0600

    1:02 carrito

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
-  <h1>Carrito</h1>
+  <div class="container">
+    <h1>Carrito</h1>
+    {{ carrito }}
+    <hr />
+    <div class="row">
+      <Card
+        v-for="producto in productos"
+        :key="producto.id"
+        :producto="producto"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import { useStore } from "vuex";
+import { computed, onMounted } from "vue";
+import Card from "./components/Card.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    Card,
+  },
+
+  setup() {
+    const store = useStore();
+    onMounted(() => {
+      store.dispatch("fetchData");
+    });
+
+    const productos = computed(() => store.state.productos);
+    const carrito = computed(() => store.state.carrito);
+    return { productos, carrito };
+  },
 };
 </script>
-
-<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,37 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    productos: [],
+    carrito: {},
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    setProducto(state, payload) {
+      state.productos = payload;
+    },
+    setCarrito(state, payload) {
+      state.carrito[payload.id] = payload;
+    },
   },
   actions: {
+    // Llamado al servidor
+    async fetchData({ commit }) {
+      try {
+        const res = await fetch("api.json");
+        const data = await res.json();
+        commit("setProducto", data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    agregarCarrito({ commit, state }, producto) {
+      state.carrito.hasOwnProperty(producto.id)
+        ? (producto.cantidad = state.carrito[producto.id].cantidad + 1)
+        : (producto.cantidad = 1);
+      commit("setCarrito", producto);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
